13edb75428d38fc2833a1ee628ba061ea89f220c
Alandelon Bispo <88351203+AlanBispo@users.noreply.github.com>
2022-01-06 13:50:37 -0300

    all files

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+
+import wordList from './resources/worlds.json';
+import Word from './components/Word'
+
+const MAX_TYPED_KEYS = 30;
+
+const getWord = () => {
+  const index = Math.floor(Math.random() * wordList.length);
+  const word = wordList[index];
+  return word.toLowerCase();
+}
+
+const isValidKey = (key, word) => {
+  if(!word) return false;
+  const result = word.split('').includes(key);
+  return result;
+}
+
+const App = () => {
+  const [typedKeys, setTypedKeys] = useState([]);
+  const [validKeys, setValidKeys] = useState([]);
+  const [completedWords, setCompletedWords] = useState([]);
+  const [word, setWord] = useState('');
+
+  useEffect(() =>{
+    setWord(getWord());
+  },[]);
+
+  useEffect(() =>{
+    const wordFromValidKeys = validKeys.join('').toLocaleLowerCase();
+    if(word && word === wordFromValidKeys){
+      let newWord = null;
+      do{
+        newWord = getWord();
+      } while(completedWords.includes(newWord));
+
+      setWord(newWord);
+      setValidKeys([]);
+      setCompletedWords((prev) => [...prev, word]);
+
+    }
+  }, [word, validKeys])
+
+  // e = event
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+    const { key } = e;
+    setTypedKeys((prev) => [...prev,key].slice(MAX_TYPED_KEYS * -1))
+
+    if(isValidKey(key, word)) {
+        setValidKeys((prev) => {
+          const isValidLength = prev.length <= word.length;
+          const isNextChar = isValidLength && word[prev.length] === key;
+          return isNextChar ? [...prev, key] : prev;
+        });
+      }
+    }
+
+  return (
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
+      <div className="valid-keys">
+        <Word word={word} validKeys={validKeys} />
+      </div>
+      <div className="typed-keys">{typedKeys ? typedKeys.join(' ') : null}</div>
+      <div className="completed-words">
+        <ol>
+          {completedWords.map((word) => (
+            <li key={word}>{word}</li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default App;
